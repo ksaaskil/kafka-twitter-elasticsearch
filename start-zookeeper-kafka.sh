e1ff4bbc453c84ead58f5cacbb65af7a614974db
Kimmo Sääskilahti <kimmo@meeshkan.com>
2020-02-05 16:22:47 +0200

    Wait for zookeeper.

--- start-zookeeper-kafka.sh
+++ start-zookeeper-kafka.sh
@@ -6,5 +6,6 @@ set -uex
 KAFKA_DIR=${HOME}/kafka_2.12-2.4.0
 
 bash ${KAFKA_DIR}/bin/zookeeper-server-start.sh ${KAFKA_DIR}/config/zookeeper.properties > zookeeper.log &
-
+echo "Waiting for zookeeper..."
+sleep 10
 bash ${KAFKA_DIR}/bin/kafka-server-start.sh ${KAFKA_DIR}/config/server.properties > kafka.log &
